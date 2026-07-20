ee89d86b16d415c65a2dd059efe31e9080a38215
Flávio Ramos <flavio.notargiacomo.ramos@gmail.com>
2026-07-20 18:48:02 -0300

    criando componente home

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,24 @@
 import { Component, OnInit, signal } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ReactiveFormsModule, FormsModule],
+  imports: [RouterOutlet, ReactiveFormsModule, FormsModule, MatCardModule],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App implements OnInit {
   protected readonly title = signal('flexpunkd20');
 
+  constructor(private readonly router: Router) {}
+
   ngOnInit() {}
+
+  navigateTo(route: string): void {
+    this.router.navigate([route]);
+  }
 }
