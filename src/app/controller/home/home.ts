322d17c9f97f24df023bebf6fb9280f46d23f3a4
Flávio Ramos <flavio.notargiacomo.ramos@gmail.com>
2026-07-24 13:22:04 -0300

    finalização da arte da página dos atributos

--- src/app/controller/home/home.ts
+++ src/app/controller/home/home.ts
@@ -3,10 +3,13 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatCard } from '@angular/material/card';
 import { AtributoPersonagem, Attribute } from '../../components/attribute/attribute';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-calculo-atributos',
-  imports: [Attribute, MatCard],
+  standalone: true,
+  imports: [Attribute, MatCard, MatTabsModule, MatButtonModule],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
@@ -36,6 +39,12 @@ export class Home implements OnInit {
   }
 
   ngOnInit(): void {
+    this.limparAtributos();
+
+    this.form = this.fb.group({});
+  }
+
+  limparAtributos() {
     this.for = { nome: 'FORÇA', comprado: 0, racial: 0, outros: 0, total: 0 };
     this.des = { nome: 'DESTREZA', comprado: 0, racial: 0, outros: 0, total: 0 };
     this.con = { nome: 'CONSTITUIÇÃO', comprado: 0, racial: 0, outros: 0, total: 0 };
@@ -44,6 +53,6 @@ export class Home implements OnInit {
     this.car = { nome: 'CARISMA', comprado: 0, racial: 0, outros: 0, total: 0 };
     this.sor = { nome: 'SORTE', comprado: 0, racial: 0, outros: 0, total: 0 };
 
-    this.form = this.fb.group({});
+    this.points = 0;
   }
 }
